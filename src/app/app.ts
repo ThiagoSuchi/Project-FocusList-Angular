5f1d6c674dd76bc8a1023581c75c8c392873f9f7
ThiagoSuchi <thiagohenssuchi@gmail.com>
2026-04-27 17:51:47 -0400

    Atualiza src/app/app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Page } from './components/shared/page/page';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Page],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
