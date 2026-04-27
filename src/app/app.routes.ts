e4dfdeaafa819c8285e26599dd2293d209983e57
ThiagoSuchi <thiagohenssuchi@gmail.com>
2026-04-27 17:51:47 -0400

    Atualiza src/app/app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Today } from './screens/today/today';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: Today
+    },
+    {
+        path: 'my-profile',
+        loadComponent: () => import('./screens/my-profile/my-profile').then(m => m.MyProfile)
+    }
+];
