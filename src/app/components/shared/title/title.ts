560d072c6e73c87efa503fcf908b9f8f263dc9fe
ThiagoSuchi <thiagohenssuchi@gmail.com>
2026-04-28 17:27:51 -0400

    feat(title): adiciona flexibilidade p/ (size, font)

--- src/app/components/shared/title/title.ts
+++ src/app/components/shared/title/title.ts
@@ -9,4 +9,6 @@ import { Component, input } from '@angular/core';
 export class Title {
   readonly main = input.required<string>();
   readonly sub = input<string>();
+  readonly size = input<string>('text-2xl');
+  readonly font = input<string>('font-extrabold');
 }
