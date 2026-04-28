5cd3f045e84ba2eaa63c29e04ca7fc7de02c606d
ThiagoSuchi <thiagohenssuchi@gmail.com>
2026-04-28 17:28:43 -0400

    refactor(task): padroniza interacoes iniciais de manipulacao em services

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,8 +1,29 @@
 // Lógica de negócio e persistência (localStorage)
 
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
+import { Task } from '../models/task.models';
 
 @Injectable({
   providedIn: 'root',
 })
-export class TaskService {}
+export class TaskService {
+  private readonly _tasks = signal<Task[]>([]);
+
+  readonly tasks = this._tasks.asReadonly();
+
+  constructor() {}
+
+  postTask(title: string) {
+    const newTask: Task = {
+      id: crypto.randomUUID(),
+      title,
+      completed: false
+    };
+
+    this._tasks.update(t => [...t, newTask]);
+  }
+
+  taskCompleted(task: Task) {
+    task.completed = !task.completed;
+  }
+}
