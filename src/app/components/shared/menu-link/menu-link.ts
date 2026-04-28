bee653d54c8ecac4db86532c9a16ad49d02d9e6a
ThiagoSuchi <thiagohenssuchi@gmail.com>
2026-04-28 17:26:40 -0400

    feat(menu-link): adiciona input icon

--- src/app/components/shared/menu-link/menu-link.ts
+++ src/app/components/shared/menu-link/menu-link.ts
@@ -1,13 +1,14 @@
 import { Component, input } from '@angular/core';
-import { RouterLink } from "@angular/router";
+import { RouterLink, RouterLinkActive } from "@angular/router";
 
 @Component({
   selector: 'app-menu-link',
-  imports: [RouterLink],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './menu-link.html',
   styleUrl: './menu-link.css',
 })
 export class MenuLink {
   readonly label = input.required<string>();
   readonly route = input.required<string>();
+  readonly icon = input.required<string>();
 }
