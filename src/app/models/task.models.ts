fc220c5e857dc30e93f0f8f2889df0eb57f7a291
ThiagoSuchi <thiagohenssuchi@gmail.com>
2026-05-16 15:30:52 -0400

    feat: Adicionar dtos para resposta e para método delete

--- src/app/models/task.models.ts
+++ src/app/models/task.models.ts
@@ -1,7 +1,16 @@
 // Interface que define a estrutura de uma tarefa
 
-export interface Task {
+export interface ITask {
     id: string,
     title: string,
     completed: boolean
+}
+
+export interface ITaskDTO {
+    title: string 
+    completed: boolean  
+}
+
+export interface IDeleteRes {
+    message: string
 }
