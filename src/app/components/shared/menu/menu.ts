62ea9906a55e5f5ad9dc29a66383723bd76e4a9e
ThiagoSuchi <thiagohenssuchi@gmail.com>
2026-05-16 15:29:48 -0400

    chore: Remover RouterLink de imports

--- src/app/components/shared/menu/menu.ts
+++ src/app/components/shared/menu/menu.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { MenuLink } from '../menu-link/menu-link';
 import { Title } from "../title/title";
-import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-menu',
-  imports: [MenuLink, Title, RouterLink],
+  imports: [MenuLink, Title],
   templateUrl: './menu.html',
   styleUrl: './menu.css',
   host: {
