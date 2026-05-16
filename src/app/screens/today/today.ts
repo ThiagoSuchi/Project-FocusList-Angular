84d71282fa2e8d47e076a00d2d2cb6c3e0c8a314
ThiagoSuchi <thiagohenssuchi@gmail.com>
2026-05-16 15:34:05 -0400

    feat: Integração de HttpClient para consumir API, uso de subscribe, método ngOnInit inicializado

--- src/app/screens/today/today.ts
+++ src/app/screens/today/today.ts
@@ -1,6 +1,7 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { Title } from "../../components/shared/title/title";
 import { TaskService } from '../../services/task.service';
+import type { ITask } from '../../models/task.models';
 
 @Component({
   selector: 'app-today',
@@ -8,18 +9,46 @@ import { TaskService } from '../../services/task.service';
   templateUrl: './today.html',
   styleUrl: './today.css',
 })
-export class Today {
+export class Today implements OnInit {
+  tasks: ITask[] = [];
   readonly taskService = inject(TaskService);
+
+  ngOnInit(): void {
+    this.getAllTask();
+  }
+
+  getAllTask(): void {
+    this.taskService.getAllTask().subscribe({
+      next: (data) => this.tasks = data,
+      error: (err) => console.error(err)
+    });
+  }
 
   postTask(event: Event, valueInput: HTMLInputElement) {
     event.preventDefault();
 
-    const inputElement = valueInput;
-
-    const title = inputElement.value.trim();
+    const title = valueInput.value.trim();
     if (!title) return;
 
-    this.taskService.postTask(title);
-    inputElement.value = '';
+    this.taskService.postTask({ title, completed: false }).subscribe({
+      next: (createdTask) => {
+        this.tasks = [...this.tasks, createdTask];
+        valueInput.value = '';
+      },
+      error: (err) => console.error('Erro ao criar task', err)
+    });
+  }
+
+  taskCompleted(task: ITask): void {
+    this.taskService.taskCompleted(task).subscribe({
+      next: () => {
+        const t = this.tasks.find(t => t.id === task?.id);
+        if (t) t.completed = !t.completed;
+      } // Atualiza localmente, recarrega do banco
+    })
+  }
+
+  deleteTask(id: string) {
+    this.taskService.deleteTask(id).subscribe(() => this.getAllTask());
   }
 }
